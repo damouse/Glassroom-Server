32cbd66efa331c955e35b4d8009565fd2deb3fe0
jholt2 <jholt2@wisc.edu>
2014-03-27 05:52:19 -0500

    added subjects to menu, waiting on user/subject relations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def getUserSubjects
+    @subjects = Subjects.all
+    @user_subjects = @subjects.select do |subject|
+      subject.user_id.include? current_user.id
+    end
+  end
 end
