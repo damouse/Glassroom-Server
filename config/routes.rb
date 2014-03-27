26e839dd6ee94777e1d8c948de7327ee913462b4
damouse <mickey.barboi@gmail.com>
2014-03-27 04:31:54 -0500

    Added Audio, Video, Image, and Note models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 GlassroomProto::Application.routes.draw do
+  
   devise_for :users
 
   root  'landing#home'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140302235927) do
+ActiveRecord::Schema.define(version: 20140327093143) do
+
+  create_table "audios", force: true do |t|
+    t.string   "name"
+    t.integer  "lecture_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "images", force: true do |t|
+    t.string   "name"
+    t.integer  "lecture_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "lectures", force: true do |t|
+    t.string   "name"
+    t.integer  "subject_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "notes", force: true do |t|
+    t.string   "name"
+    t.string   "text"
+    t.integer  "lecture_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "subjects", force: true do |t|
     t.string   "name"
@@ -39,5 +68,12 @@ ActiveRecord::Schema.define(version: 20140302235927) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "videos", force: true do |t|
+    t.string   "name"
+    t.integer  "lecture_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
