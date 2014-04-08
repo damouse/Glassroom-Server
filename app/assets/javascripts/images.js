ec2cae5c13dfd3f4fb4d4b09414fb0f22537f847
Yucheng Tu <yuchengtu.uwm@gmail.com>
2014-04-08 01:22:18 -0500

    js for sortable

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,5 +1,24 @@
 //= require jquery-ui
   $(function() {
-    $( "#lecture-img-sortable" ).sortable();
+    $( "#lecture-img-sortable" ).sortable({
+    	update: function (event, ui) {
+ 
+                           var list =  $(this).sortable("toArray").join("|");
+                           var currentID = this.id;
+                           //var order = $(this).index(currentID);
+                           /*
+                           $.ajax({   url: "/persistListOrder.php",
+                                      data: { 
+                                                  'section':this.id,              
+                                                  'components': list
+                                            }
+                                  });
+*/
+						console.log(list);
+                  }
+    });
     $( "#lecture-img-sortable" ).disableSelection();
+  });
+  $("#change-img-order").click(function(){
+  	$("form").submit();
   });
