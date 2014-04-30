51efa8c6a62a2698fd8ba7b4acf40ad84667c9e3
Yucheng Tu <yuchengtu.uwm@gmail.com>
2014-04-30 17:17:51 -0500

    moved create note form outside ul

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -1,58 +1,58 @@
 //= require jquery.hotkeys
 //= require bootstrap-wysiwyg
- $(function(){
+$(function(){
 
 /*
-  function initToolbarBootstrapBindings(bindTarget) {
-      var fonts = ['Serif', 'Sans', 'Arial', 'Arial Black', 'Courier', 
-            'Courier New', 'Comic Sans MS', 'Helvetica', 'Impact', 'Lucida Grande', 'Lucida Sans', 'Tahoma', 'Times',
-            'Times New Roman', 'Verdana'],
-            fontTarget = $('[title=Font]').siblings('.dropdown-menu');
-      $.each(fonts, function (idx, fontName) {
-          fontTarget.append($('<li><a data-edit="fontName ' + fontName +'" style="font-family:\''+ fontName +'\'">'+fontName + '</a></li>'));
-      });
-      $('a[title]').tooltip({container:'body'});
-    	$('.dropdown-menu input').click(function() {return false;})
-		    .change(function () {$(this).parent('.dropdown-menu').siblings('.dropdown-toggle').dropdown('toggle');})
-        .keydown('esc', function () {this.value='';$(this).change();});
+  function initToolbarBootstrapBindings() {
+    var fonts = ['Serif', 'Sans', 'Arial', 'Arial Black', 'Courier', 
+    'Courier New', 'Comic Sans MS', 'Helvetica', 'Impact', 'Lucida Grande', 'Lucida Sans', 'Tahoma', 'Times',
+    'Times New Roman', 'Verdana'],
+    fontTarget = $('[title=Font]').siblings('.dropdown-menu');
+    $.each(fonts, function (idx, fontName) {
+      fontTarget.append($('<li><a data-edit="fontName ' + fontName +'" style="font-family:\''+ fontName +'\'">'+fontName + '</a></li>'));
+    });
+    $('a[title]').tooltip({container:'body'});
+    $('.dropdown-menu input').click(function() {return false;})
+    .change(function () {$(this).parent('.dropdown-menu').siblings('.dropdown-toggle').dropdown('toggle');})
+    .keydown('esc', function () {this.value='';$(this).change();});
 
-      $('[data-role=magic-overlay]').each(function () { 
-        var overlay = $(this), target = $(overlay.data('target')); 
-        overlay.css('opacity', 0).css('position', 'absolute').offset(target.offset()).width(target.outerWidth()).height(target.outerHeight());
-      });
-      if ("onwebkitspeechchange"  in document.createElement("input")) {
-        var editorOffset = $('#editor').offset();
-        $('#voiceBtn').css('position','absolute').offset({top: editorOffset.top, left: editorOffset.left+$('#editor').innerWidth()-35});
-      } else {
-        $('#voiceBtn').hide();
-      }
-	};
-	function showErrorAlert (reason, detail) {
-		var msg='';
-		if (reason==='unsupported-file-type') { msg = "Unsupported format " +detail; }
-		else {
-			console.log("error uploading file", reason, detail);
-		}
-		$('<div class="alert"> <button type="button" class="close" data-dismiss="alert">&times;</button>'+ 
-		 '<strong>File upload error</strong> '+msg+' </div>').prependTo('#alerts');
-	};
+    $('[data-role=magic-overlay]').each(function () { 
+      var overlay = $(this), target = $(overlay.data('target')); 
+      overlay.css('opacity', 0).css('position', 'absolute').offset(target.offset()).width(target.outerWidth()).height(target.outerHeight());
+    });
+    if ("onwebkitspeechchange"  in document.createElement("input")) {
+      var editorOffset = $('#editor').offset();
+      $('#voiceBtn').css('position','absolute').offset({top: editorOffset.top, left: editorOffset.left+$('#editor').innerWidth()-35});
+    } else {
+      $('#voiceBtn').hide();
+    }
+  };
+  function showErrorAlert (reason, detail) {
+    var msg='';
+    if (reason==='unsupported-file-type') { msg = "Unsupported format " +detail; }
+    else {
+     console.log("error uploading file", reason, detail);
+   }
+   $('<div class="alert"> <button type="button" class="close" data-dismiss="alert">&times;</button>'+ 
+     '<strong>File upload error</strong> '+msg+' </div>').prependTo('#alerts');
+ };
 
-  initToolbarBootstrapBindings();
+ initToolbarBootstrapBindings();
   //$(".btn-toolbar").attr("data-target", "#editor");  
-	$('#editor').wysiwyg({ fileUploadError: showErrorAlert} );
+  $('#editor').wysiwyg({ fileUploadError: showErrorAlert} );
   window.prettyPrint && prettyPrint();
   */
-$("#create_new_note_form").on("ajax:success", function(event){
-  event.preventDefault();
-  console.log("note create succ");
-  $(".new_note_name").val('');
-  $(".new_note_body").val('');
+  $("#create_new_note_form").on("ajax:success", function(event){
+    event.preventDefault();
+    console.log("note create succ");
+    $(".new_note_name").val('');
+    $(".new_note_body").val('');
   //location.reload();
   /*
   $('#courses_ajax').append("<%= escape_javascript(
   render :partial => "your_partial", :object => your_varable, 
   :locals => {:your_other_var => value, ...}  %>");
-*/
+  */
 });
 
 
